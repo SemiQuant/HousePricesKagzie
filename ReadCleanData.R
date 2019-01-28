3874433dfb20697cea4364675289ef8d635ce339
SemiQuant <lmbjas002@myuct.ac.za>
2019-01-28 20:24:21 +0200

    read data

---
+++ ReadCleanData.R
@@ -0,0 +1,13 @@
+# read in data from Kaggle https://www.kaggle.com/c/house-prices-advanced-regression-techniques
+dat.train <- read.csv("data/train.csv")
+dat.Test <- read.csv("data/test.csv")
+str(Train)
+str(Test)
+
+# figure how to read this in
+system("cat data/data_description.txt")
+
+
+# head(read.csv("data/sample_submission.csv"))
+
+
