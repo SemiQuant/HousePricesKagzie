391523c9d3763b7f3bc0a32dc5547e446245e5ca
R-Noob <de.mcclllnd@gmail.com>
2019-02-01 11:56:00 +0200

    y variable transform

--- ReadCleanData.R
+++ ReadCleanData.R
@@ -1,5 +1,3 @@
-# This is my first change!
-
 # Load packages and set some things up #
 require(doParallel)
 set.seed(1987)
@@ -22,8 +20,11 @@ str(dat.train)
 str(dat.test)
 
 # figure how to read this in
-system("cat data/data_description.txt")
+# system("cat data/data_description.txt")
 # how we read this file in nice? idk what format it is..
+
+# Is this what you mean? I think the best we can do is read as csv and the format wide if we want?
+data_description <- read.csv("D:/Research/R learning/Kaggle/data/data_description.txt", stringsAsFactors = FALSE)
 
 
 # head(read.csv("data/sample_submission.csv"))
@@ -49,81 +50,144 @@ aggr(missing, numbers = TRUE, prop = c(TRUE, FALSE))
 )
 
 # Alley
-system("grep -A 5 Alley data/data_description.txt")
+# system("grep -A 5 Alley data/data_description.txt")
 # looks like the missing ally means none, so lets change it to "none" for now
 
+
 # Lets see all
-system("grep NA data/data_description.txt")
+# system("grep NA data/data_description.txt")
+
+# Instead of being super fancy:
+View(data_description[[1]][grep("NA", data_description[[1]])])
+
 
 # so all missing that are factor are none
-missing <- dat.train %>%
-  select_if(function(x) any(is.na(x)))%>%
-  select_if(function(x) is.factor(x))
+# missing <- dat.train %>%
+#   select_if(function(x) any(is.na(x)))%>%
+#   select_if(function(x) is.factor(x))
+
+missing <- as.data.frame(
+  dat.train %>%
+    select_if(function(x) {any(is.na(x)) & is.factor(x)}) %>%
+    apply(., 2, function(y){
+      recode_factor(y, .missing = "None")})
+)
 
 dat.train <- dat.train[!colnames(dat.train)%in%colnames(missing)]
-
-missing[] <- lapply(missing, as.character)
-missing[is.na(missing)] <- "None"
-missing[] <- lapply(missing, as.factor)
 
 #think its in the same orde, bas code you can clean :)
 dat.train <- bind_cols(dat.train, data.frame(missing))
 
 
-# these are trule missing?
+# these are truly missing?
 colnames(dat.train %>%
-       select_if(function(x) any(is.na(x))))
+           select_if(function(x) any(is.na(x))))
 
-system("grep -A 2 -B 2 GarageYrBlt data/data_description.txt")
+# Lets check
+
+apply(dat.train %>%
+        select_if(function(x) {any(is.na(x))})
+      , 2, min, na.rm = TRUE)
+
+# Lot Frontage not a panhandle?
+
+# No idea what masonry veneer area is but it looks like truly missing because min value is zero
+
+# Garage Year Built is NA when there is no Garage but its not missing data
+identical(
+  dat.train$GarageYrBlt[dat.train$GarageYrBlt == "NA"] == "NA",
+  dat.train$GarageYrBlt[dat.train$GarageType == "None"] == "NA"
+)
+
+# ok so same conclusion as below, your method is better but choice of variable is worse :)
+
+# system("grep -A 2 -B 2 GarageYrBlt data/data_description.txt")
 table(is.na(dat.train$GarageYrBlt), dat.train$GarageCars==0)
 #so this isnt there as they is no garage
 
-system("grep LotFrontage data/data_description.txt")
-table(lotFront = is.na(dat.train$LotFrontage), lotArea = dat.train$LotArea>0)
+# system("grep LotFrontage data/data_description.txt")
+# table(lotFront = is.na(dat.train$LotFrontage), lotArea = dat.train$LotArea>0)
 # ah, one truley missing! Lets impute this later
+# Nope, lot area doesn't need frontage
 
-system("grep Mas data/data_description.txt")
+
+# system("grep Mas data/data_description.txt")
 table(is.na(dat.train$MasVnrArea), dat.train$MasVnrType)
 
-aggr(dat.train %>%
-       select_if(function(x) any(is.na(x))),
-     numbers = TRUE, prop = c(TRUE, FALSE))
 
+# But why only 8 out of 864 have missing values? surely these should be zero if masonry veneer type is "none"?
 
-# so we will impute LotFrontage
-# GarageYrBlt and MasVnrArea must be dealt with but not imputed
+View(dat.train$MasVnrArea[dat.train$MasVnrType == "None"])
 
-#something like this - thinking just using the Lot data for the lot imputation?
-# require(mice)
-methods(mice)
-tempData <- mice(dat.train[grepl("Lot", colnames(dat.train))], m=5, maxit=50, meth='cart', seed=1987)
-summary(tempData)
-densityplot(tempData)
-stripplot(tempData, pch = 20, cex = 1.2)
-
-completedData <- complete(tempData, 2)
-table(is.na(completedData))
-dim(completedData)
-
-dat.train$LotFrontage <- completedData$LotFrontage
-
+# Ok very confusing. All zero except for the NAs and like 5 weird outliers...
 
 aggr(dat.train %>%
        select_if(function(x) any(is.na(x))),
      numbers = TRUE, prop = c(TRUE, FALSE))
 
-dat.train <- dat.train[-c(1)]
+# Can we delete the imputations?
+# Also should we make NA LotFrontage 0?
+# What should we do about Mason Veneer Area? Surely if type is none then area should be none? Or does that mean there is allocated space for mason veneer that's not used? like an untiled bathroom that needs tiles?
+
+# # so we will impute LotFrontage
+# # GarageYrBlt and MasVnrArea must be dealt with but not imputed
+#
+# #something like this - thinking just using the Lot data for the lot imputation?
+# # require(mice)
+# methods(mice)
+# tempData <- mice(dat.train[grepl("Lot", colnames(dat.train))], m=5, maxit=50, meth='cart', seed=1987)
+# summary(tempData)
+# densityplot(tempData)
+# stripplot(tempData, pch = 20, cex = 1.2)
+#
+# completedData <- complete(tempData, 2)
+# table(is.na(completedData))
+# dim(completedData)
+#
+# dat.train$LotFrontage <- completedData$LotFrontage
+#
+#
+# aggr(dat.train %>%
+#        select_if(function(x) any(is.na(x))),
+#      numbers = TRUE, prop = c(TRUE, FALSE))
+#
+# dat.train <- dat.train[-c(1)]
 
 ################
 
+# Dave's exploratory data analysis
+# Lets first look at Y variable.
+
+density_y <- density(y.train)
+hist(y.train, plot = TRUE)
+plot(density_y)
+
+# Heavily right skewed. Looks like it may be log normally distributed. Try log transform?
+hist(log(y.train), plot = TRUE)
+density_logy <- density(log(y.train))
+plot(density_logy)
+
+?ks.test
+# Ok looks much better. Lets test for normality. P-Value will tell us the probability that this data comes from a normal distribution.
+
+shapiro.test(log(y.train))
+
+# Ok so not normal...
+
+# Lets look at skewness and kurtosis stats:
+# library(moments)
+
+skewness(log(y.train))
+kurtosis(log(y.train))
+
+# Skewness seems like less of a problem than kurtosis. skew should be 0 kurt should be 3. We can look at box-cox transform or ignore because its at least approx normal and review residuals or regression.
 
 
 
 
 
 
-
-everything below I jsut took from some of my other scripts, havent changed anything yet
+# everything below I jsut took from some of my other scripts, havent changed anything yet
 
 
 
@@ -146,6 +210,9 @@ featurePlot(x = dat.train[,3:4],
             # plot = "scatter",
             auto.key = list(columns = 3)
 )
+
+# Can't get this to work with all the plots? only the first plot works when run alone? But I have no idea how to read this...
+
 
 # Identifying Correlated Predictors
 # require(corrplot)
